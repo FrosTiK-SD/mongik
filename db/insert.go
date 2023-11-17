48955049203468036c69cdf129334429842ef155
Apple <apple@Apples-MacBook-Pro.local>
2023-11-17 16:16:37 +0530

    Improved insertmany

--- db/insert.go
+++ db/insert.go
@@ -7,16 +7,21 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func InsertOne[Doc any](mongikClient *mongik.Mongik, db string, collectionName string, doc Doc) (*mongo.InsertOneResult, error) {	
+func InsertOne[Doc any](mongikClient *mongik.Mongik, db string, collectionName string, doc Doc) (*mongo.InsertOneResult, error) {
 	// Query to DB
 	docId, err := mongikClient.MongoClient.Database(db).Collection(collectionName).InsertOne(context.Background(), doc)
 
 	DBCacheReset(mongikClient.CacheClient, collectionName)
 	return docId, err
 }
-func InsertMany[Result any](mongikClient *mongik.Mongik, db string, collectionName string, doc []interface{}) (*mongo.InsertManyResult, error) {	
+func InsertMany[Doc any](mongikClient *mongik.Mongik, db string, collectionName string, docs []Doc) (*mongo.InsertManyResult, error) {
+	// Convert []struct to []interface
+	docsInterface := make([]interface{}, len(docs))
+	for index := range docs {
+		docsInterface[index] = docs[index]
+	}
 	// Query to DB
-	docIds, err := mongikClient.MongoClient.Database(db).Collection(collectionName).InsertMany(context.Background(), doc)
+	docIds, err := mongikClient.MongoClient.Database(db).Collection(collectionName).InsertMany(context.Background(), docsInterface)
 
 	DBCacheReset(mongikClient.CacheClient, collectionName)
 	return docIds, err
